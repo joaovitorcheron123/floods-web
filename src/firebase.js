4a86a009f6eeef02b5c69aeca48747b149b5c057
João Vitor Cheron <55923765+joaovitorcheron123@users.noreply.github.com>
2021-06-21 18:21:51 -0300

    Update firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,7 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBVIj1qx6BWAdtnLgZSbmNv0Dlp9Xx9AtU",
-  authDomain: "floods-web.firebaseapp.com",
-  projectId: "floods-web",
-  storageBucket: "floods-web.appspot.com",
-  messagingSenderId: "978059909266",
-  appId: "1:978059909266:web:f6aa0a48656e5fd18c6e4b"
+//
 };
   
   const firebaseApp = firebase.initializeApp(firebaseConfig);
